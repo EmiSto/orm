b85f1510102c761b98c00f2fec12e41255d7ba2f
Axel Dalbom <axel.dbom@gmail.com>
2017-12-10 14:17:53 +0100

    frukter spawnar överallt

--- Parser.java
+++ Parser.java
@@ -7,9 +7,9 @@ public class Parser
 	ArrayList<String> data = new ArrayList();
 	char cursor;
 	String newElement = "";
-	data.add(string.charAt(0) + "");
+	//data.add(string.charAt(0) + "");
 	
-	for(int i = 2; i<string.length(); i++){
+	for(int i = 0; i<string.length(); i++){
 	    cursor = string.charAt(i);
 	    if(cursor != ';'){
 		newElement += cursor;
